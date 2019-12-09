eaa8fe6dcd92c76429b4bdaaa1d87043217dd067
1552284258 <1552284258@qq.com>
2019-12-09 21:33:39 +0800

    wx

---
+++ jingdong/src/components/shopcat/footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <van-submit-bar :price="3050" button-text="提交订单" @submit="onSubmit">
+      <van-checkbox v-model="checked">全选</van-checkbox>
+    </van-submit-bar>
+  </footer>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+  name: "XXX",
+  data() {
+    return {
+      checked:[]
+    };
+  },
+  components: {},
+  methods: {
+    onSubmit() {
+      
+    }
+  },
+};
+</script>
+<style lang="less">
+footer {
+  z-index: 999;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  .van-checkbox {
+    margin-left: 3vw;
+  }
+}
+</style>
---
+++ jingdong/src/components/shopcat/menu.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="menu" v-show="menu">
+    <router-link to="/" tag="p">
+      <van-icon name="wap-home-o" />
+      首页
+    </router-link>
+    <router-link to="/" tag="p">
+      <van-icon name="search" />
+      搜索
+    </router-link>
+    <router-link to="/shopcat" tag="p">
+      <van-icon name="shopping-cart-o" />
+      购物车
+    </router-link>
+    <router-link to="/" tag="p">
+      <van-icon name="contact" />
+      我的
+    </router-link>
+    <router-link to="/" tag="p">
+      <van-icon name="todo-list-o" />
+      浏览记录
+    </router-link>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+  name: "XXX",
+  data() {
+    return {
+      menu:false
+    };
+  },
+  components: {}
+};
+</script>
+<style lang="less">
+.menu {
+  position: absolute;
+  top: 10vw;
+  right: 0;
+  width: 30vw;
+  text-align: left;
+  background: rgba(0, 0, 0, 0.9);
+  color: #fff;
+  border-radius: 10px;
+  > p {
+    margin-left: 3vw;
+    display: block;
+    border-bottom: 1px solid #eee;
+    height: 7vw;
+    line-height: 7vw;
+  }
+}
+</style>
