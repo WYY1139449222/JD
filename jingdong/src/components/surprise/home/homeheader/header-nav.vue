8ac1f67139f17a14a6084f15d2fb8abd988cd288
Fyh0706 <54092244+Fyh0706@users.noreply.github.com>
2019-12-09 18:04:04 +0800

    更新node

---
+++ jingdong/src/components/surprise/home/homeheader/header-nav.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="header-nav">
+    <div class="header">
+      <div>
+        <van-icon name="bag" />
+      </div>
+      <div>
+        <van-search placeholder="大家都在搜地毯" v-model="value" background="none" />
+      </div>
+      <div>
+        <van-icon name="bag" />
+      </div>
+    </div>
+    <div class="nav">
+      <div>精选</div>
+      <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
+      <div>
+        <van-icon name="arrow-down" />
+      </div>
+    </div>
+    <header-banner></header-banner>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import headerbanner from "./banner";
+export default {
+  name: "header-nav",
+  data() {
+    return {
+      value: ""
+    };
+  },
+  components: {
+    "header-banner": headerbanner
+  }
+};
+</script>
+<style lang="less" scoped>
+.header-nav {
+  padding: 5vw;
+  // position: relative;
+  // padding: 0;
+  background: url(http://img13.360buyimg.com/jdphoto/jfs/t1/92261/22/3369/194260/5ddf6dbceffb2bdcd/34b7bfd4cb2db340.png!q70.webp);
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+  .nav {
+    justify-content: space-between;
+    ul > li {
+      float: left;
+    }
+    display: flex;
+  }
+}
+</style>
---
+++ jingdong/src/components/surprise/home/homeheader/header.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+           <van-icon name="arrow-left" class="headicon" @click="backFn" /> 
+        </div>
+        <div>惊喜</div>
+        <div class="header-right">...</div>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+    name: 'HOMEheader',
+    data() {
+        return {
+        
+        }
+    },
+    components: {
+        
+    },methods: {
+        backFn(){
+            this.$router.go(-1);
+        }
+    },
+}
+</script>
+<style lang="less" scoped>
+ .header{
+    //  padding: 10px;
+     font-size: 5vw;
+     display: flex;
+     justify-content:space-between ;
+     text-align: center;
+     vertical-align: middle;
+     height: 13vw;
+     line-height: 13vw;
+     padding: 0 5vw;
+     .headicon{
+           padding-top: 0.5vw;
+           line-height: 13vw;
+     }
+   
+//   .header-left{
+//       padding: 1.5vw;
+//   }
+ }
+</style>
