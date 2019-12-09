c43770be5e17375589f1ee4d8f2f005cf181c431
WYY1139449222 <54354280+WYY1139449222@users.noreply.github.com>
2019-12-09 20:38:11 +0800

    aa

--- jingdong/src/components/classify/navlift.vue
+++ jingdong/src/components/classify/navlift.vue
@@ -1,22 +1,65 @@
 <template>
-    <div>
-        左边nav
+  <div class="classify">
+    <div class="navLiftBox">
+      <van-sidebar v-model="activeKey" class="navLift">
+        <van-sidebar-item
+          :title="item.title"
+          v-for="(item,index) in data.title"
+          :key="index"
+          :class="{cur:activeKey==index}"
+          @click="changelist"
+        />
+      </van-sidebar>
     </div>
+    <particulars :data='data.details'></particulars>
+  </div>
 </template>
 <script>
 // @ is an alias to /src
+import particulars from "./particulars";
 export default {
-    name: 'XXX',
-    data() {
-        return {
-        
-        }
-    },
-    components: {
-        
+  name: "XXX",
+  data() {
+    return {
+      activeKey: 0
+    };
+  },
+  props: ["data"],
+  components: {
+      particulars
+  },
+  methods: {
+    changelist() {
+        this.particularsList.sort((a,b)=>{
+            return Math.random()-0.5
+        })
     }
-}
+  },
+  computed: {
+      particularsList(){
+          return this.data.details
+      }
+  },
+};
 </script>
-<style lang="less">
-
+<style lang="less" scoped>
+.classify{
+    width: 100%;
+    overflow: hidden;
+}
+.navLiftBox {
+  width: 25vw;
+  height: 160vw;
+  overflow: scroll;
+  float: left;
+}
+.navLift {
+  width: 25vw;
+  > li {
+    height: 15vw;
+  }
+}
+.cur {
+  color: red;
+}
 </style>
--- jingdong/src/components/classify/particulars.vue
+++ jingdong/src/components/classify/particulars.vue
@@ -1,22 +1,71 @@
 <template>
-    <div>
-        右边详细
-    </div>
+  <div class="particularrsBox">
+    <header>
+      <div class="headLift">热门分类</div>
+      <div class="headright">
+        <van-icon name="chart-trending-o" color="red" />
+        <span>排行榜</span>
+        <van-icon name="arrow" color="#aaa" />
+      </div>
+    </header>
+    <van-row type="float" justify="space-around" class="particularrslist">
+      <van-col span="6" v-for="(item,index) in data" :key='index' class="list">
+          <img :src="item.img" alt="">
+          <p>{{item.til}}</p>
+      </van-col>
+     
+    </van-row>
+  </div>
 </template>
 <script>
 // @ is an alias to /src
 export default {
-    name: 'XXX',
-    data() {
-        return {
-        
-        }
-    },
-    components: {
-        
-    }
-}
+  name: "XXX",
+  data() {
+    return {};
+  },
+  props:['data'],
+  components: {}
+};
 </script>
 <style lang="less">
-
+.particularrsBox {
+  float: right;
+  width: 75vw;
+  height: 150vw;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  > header {
+    margin: 3vw 0;
+    height: 10vw;
+    line-height: 10vw;
+    .headLift {
+      float: left;
+    }
+    .headright {
+      float: right;
+      .van-icon {
+        vertical-align: text-bottom;
+      }
+      span {
+        margin: 0 1vw;
+        font-size: 3vw;
+        color: #aaa;
+      }
+    }
+  }
+  .particularrslist{
+        width: 75vw;
+        padding: 0 3vw;
+        .list{
+            width: 23vw;
+            padding: 1.5vw;
+            >img{
+                width: 20vw;
+                height: 20vw;
+            }
+        }
+    }
+   
+}
 </style>
