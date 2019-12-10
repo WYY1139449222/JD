36cc41176bfdaef79d5fc1a6afcc208c2ed5e00d
1552284258 <1552284258@qq.com>
2019-12-10 17:05:41 +0800

    a

--- jingdong/src/components/shopcat/shopcat.vue
+++ jingdong/src/components/shopcat/shopcat.vue
@@ -1,18 +1,11 @@
 <template>
   <div>
-    <my-header></my-header>
-    <my-main></my-main>
-    <footer>
-      <van-submit-bar :price="3050" button-text="提交订单" @submit="onSubmit">
-        <van-checkbox v-model="checked">全选</van-checkbox>
-      </van-submit-bar>
-    </footer>
+
   </div>
 </template>
 <script>
 // @ is an alias to /src
-import Hder from "./header.vue";
-import Min from "./main";
+
 export default {
   name: "XXX",
   data() {
@@ -21,12 +14,8 @@ export default {
     };
   },
   components: {
-    "my-header": Hder,
-    "my-main": Min
   },
-  methods: {
-    onSubmit() {}
-  },
+
   computed: {}
 };
 </script>
